fa0ecc26095833355255f370a103dbc08c69e4dc
David Dinkins <d.b.dinkins@gmail.com>
2022-11-13 12:52:52 -0500

    adds shopping cart data case and factory for reducing test setup code

---
+++ test/cases/data_case.ex
@@ -0,0 +1,184 @@
+defmodule ShoppingCart.DataCase do
+  use ExUnit.CaseTemplate
+  alias ShoppingCart.Schemas.{Cart, Customer, PaymentMethod, ShippingAddress, User}
+  alias ShoppingCart.Repo
+  alias Orders.Order
+  alias Ecto.Changeset
+
+  using do
+    quote do
+      alias Ecto.Changeset
+      import ShoppingCart.DataCase
+      alias ShoppingCart.{Factory, Repo}
+    end
+  end
+
+  def assert_valid_changeset(params, fields, changeset_fn) when is_list(fields) do
+    changeset = changeset_fn.(params)
+
+    assert %Changeset{valid?: true, changes: changes} = changeset
+
+    params = atom_map(params)
+
+    for field <- fields do
+      actual = Map.get(changes, field)
+      expected = params[field]
+
+      assert actual == expected,
+             "Values did not match for field: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+    end
+  end
+
+  def assert_valid_embedded_changeset(params, fields, changeset_fn) when is_list(fields) do
+    changeset = changeset_fn.(params)
+
+    embedded_schema_key =
+      params
+      |> Map.keys()
+      |> List.first()
+      |> String.to_atom()
+
+    assert %Changeset{valid?: true, changes: changes} = changeset
+
+    embedded_changes =
+      changes
+      |> Map.get(embedded_schema_key)
+      |> Map.get(:changes)
+
+    params =
+      atom_map(params)
+      |> Map.get(embedded_schema_key)
+      |> atom_map
+
+    for field <- fields do
+      actual = Map.get(embedded_changes, field)
+      expected = params[field]
+
+      assert actual == expected,
+             "Values did not match for field: #{field}\nexpected: #{inspect(expected)}\nactual: #{inspect(actual)}"
+    end
+  end
+
+  def assert_invalid_changeset(params, fields, changeset_fn, validation_type)
+      when is_list(fields) do
+    # changeset = Cart.create_order_changeset(params)
+    changeset = changeset_fn.(params)
+
+    assert %Changeset{
+             valid?: false,
+             errors: errors
+           } = changeset
+
+    assert_error_fields(errors, fields, validation_type)
+  end
+
+  def assert_error_fields(errors, fields, validation_type) when is_atom(validation_type) do
+    for field <- fields do
+      assert errors[field], "expected an error for #{field}"
+      {_, meta} = errors[field]
+
+      assert meta[:validation] == validation_type,
+             "The validation type, #{meta[:validation]}, is incorrect."
+    end
+  end
+
+  def schema_fields, do: Cart.fields()
+  def required_fields, do: Cart.required_fields()
+  def order_schema_fields, do: Order.fields()
+
+  def order_schema_fields(:string) do
+    Order.fields()
+    |> Enum.map(fn x -> to_string(x) end)
+  end
+
+  def order_required_schema_fields, do: Order.required_fields()
+  def skus_required_schema_fields, do: Order.skus_required_fields()
+  def address_schema_fields, do: ShippingAddress.fields()
+  def customer_schema_fields, do: Customer.fields()
+  def payment_method_schema_fields, do: PaymentMethod.fields()
+  def user_schema_fields, do: User.cart_fields()
+
+  def order_skus_required_fields do
+    Order.skus_required_fields()
+    |> Enum.map(fn x -> to_string(x) end)
+  end
+
+  def bad_cart_params do
+    %{
+      "billing_address" => 0,
+      "browser_user_agent" => %{},
+      "cookie" => %{},
+      "customer" => 0,
+      "language" => 0,
+      "order" => 0,
+      "payment_method" => "not an integer",
+      "shipping_address" => "not an integer",
+      "user" => "not an integer",
+      "user_id" => %{}
+    }
+  end
+
+  def bad_order_params do
+    %{
+      "total_transaction_price" => %{},
+      "tax_price" => %{},
+      "order_price" => %{},
+      "currency" => NaiveDateTime.utc_now(),
+      "payment_installments" => NaiveDateTime.utc_now(),
+      "payment_method" => NaiveDateTime.utc_now(),
+      "payment_country" => 0,
+      "skus" => 0,
+      "status" => 0,
+      "cart_id" => "not_an_integer"
+    }
+  end
+
+  def bad_address_params do
+    %{
+      "first_line" => %{},
+      "second_line" => %{},
+      "city" => NaiveDateTime.utc_now(),
+      "state" => NaiveDateTime.utc_now(),
+      "country" => NaiveDateTime.utc_now(),
+      "postal_code" => %{},
+      "phone_number" => %{}
+    }
+  end
+
+  def bad_customer_params do
+    %{
+      "first_name" => %{},
+      "last_name" => %{},
+      "email" => NaiveDateTime.utc_now(),
+      "phone_number" => NaiveDateTime.utc_now(),
+      "documentation_number" => NaiveDateTime.utc_now()
+    }
+  end
+
+  def bad_payment_method_params do
+    %{
+      "cc_token_id" => %{},
+      "name" => %{}
+    }
+  end
+
+  def bad_user_params do
+    %{
+      "name" => %{}
+    }
+  end
+
+  def atom_map(string_key_map) do
+    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+    end
+
+    :ok
+  end
+end
---
+++ test/factory.ex
@@ -0,0 +1,278 @@
+defmodule ShoppingCart.Factory do
+  use ExMachina.Ecto, repo: ShoppingCart.Repo
+
+  alias ShoppingCart.Schemas.{
+    Cart,
+    Customer,
+    BillingAddress,
+    PaymentMethod,
+    ShippingAddress,
+    User
+  }
+
+  alias Orders.Order
+
+  def empty_cart_no_order_factory do
+    user = insert(:user)
+
+    %Cart{
+      cookie: Faker.UUID.v4(),
+      browser_user_agent: Faker.Internet.UserAgent.desktop_user_agent(),
+      language: Enum.random(["en", "es", "pt"]),
+      billing_address: nil,
+      customer: nil,
+      order: nil,
+      payment_method: nil,
+      shipping_address: nil,
+      user_id: user.id,
+      inserted_at: NaiveDateTime.new!(~D[2010-01-13], ~T[23:00:07.005]),
+      updated_at: NaiveDateTime.new!(~D[2010-01-13], ~T[23:00:07.005])
+    }
+  end
+
+  def empty_cart_factory do
+    user = insert(:user)
+
+    %Cart{
+      cookie: Faker.UUID.v4(),
+      browser_user_agent: Faker.Internet.UserAgent.desktop_user_agent(),
+      language: Enum.random(["en", "es", "pt"]),
+      billing_address: nil,
+      customer: nil,
+      order: %{},
+      payment_method: nil,
+      shipping_address: nil,
+      user_id: user.id,
+      inserted_at: NaiveDateTime.new!(~D[2010-01-13], ~T[23:00:07.005]),
+      updated_at: NaiveDateTime.new!(~D[2010-01-13], ~T[23:00:07.005])
+    }
+  end
+
+  def cart_from_last_month_factory do
+    order = order_factory()
+    user = insert(:user)
+
+    %Cart{
+      cookie: Faker.UUID.v4(),
+      browser_user_agent: Faker.Internet.UserAgent.desktop_user_agent(),
+      language: Enum.random(["en", "es", "pt"]),
+      billing_address: %{
+        first_line: Faker.Address.street_address(),
+        second_line: Faker.Address.secondary_address(),
+        city: Faker.Address.city(),
+        state: Faker.Address.state(),
+        country: order.payment_country,
+        postal_code: Faker.Address.postcode(),
+        phone_number: Faker.Phone.EnUs.phone()
+      },
+      customer: %{
+        first_name: Faker.Person.PtBr.first_name(),
+        last_name: Faker.Person.PtBr.last_name(),
+        email: Faker.Internet.email(),
+        phone_number: Faker.Phone.EnUs.phone(),
+        documentation_number: Faker.Address.country_code()
+      },
+      inserted_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000]),
+      updated_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000]),
+      order: order_from_last_month_factory(),
+      payment_method: %{
+        name: order.payment_method
+      },
+      shipping_address: %{
+        first_line: Faker.Address.street_address(),
+        second_line: Faker.Address.secondary_address(),
+        city: Faker.Address.city(),
+        state: Faker.Address.state(),
+        country: Faker.Address.country_code(),
+        postal_code: Faker.Address.postcode(),
+        phone_number: Faker.Phone.EnUs.phone()
+      },
+      user_id: user.id,
+      user: user
+    }
+  end
+
+  def cart_factory do
+    order = order_factory()
+    user = insert(:user)
+
+    %Cart{
+      cookie: Faker.UUID.v4(),
+      browser_user_agent: Faker.Internet.UserAgent.desktop_user_agent(),
+      language: Enum.random(["en", "es", "pt"]),
+      billing_address: %{
+        first_line: Faker.Address.street_address(),
+        second_line: Faker.Address.secondary_address(),
+        city: Faker.Address.city(),
+        state: Faker.Address.state(),
+        country: order.payment_country,
+        postal_code: Faker.Address.postcode(),
+        phone_number: Faker.Phone.EnUs.phone()
+      },
+      customer: %{
+        first_name: Faker.Person.PtBr.first_name(),
+        last_name: Faker.Person.PtBr.last_name(),
+        email: Faker.Internet.email(),
+        phone_number: Faker.Phone.EnUs.phone(),
+        documentation_number: Faker.Address.country_code()
+      },
+      order: order_factory(),
+      payment_method: %{
+        name: order.payment_method
+      },
+      shipping_address: %{
+        first_line: Faker.Address.street_address(),
+        second_line: Faker.Address.secondary_address(),
+        city: Faker.Address.city(),
+        state: Faker.Address.state(),
+        country: Faker.Address.country_code(),
+        postal_code: Faker.Address.postcode(),
+        phone_number: Faker.Phone.EnUs.phone()
+      },
+      user_id: user.id,
+      user: user
+    }
+  end
+
+  def customer_factory do
+    %Customer{
+      first_name: Faker.Person.PtBr.first_name(),
+      last_name: Faker.Person.PtBr.last_name(),
+      email: Faker.Internet.email(),
+      phone_number: Faker.Phone.EnUs.phone(),
+      documentation_number: Faker.Address.country_code()
+    }
+  end
+
+  def billing_address_factory do
+    order = order_factory()
+
+    %BillingAddress{
+      first_line: Faker.Address.street_address(),
+      second_line: Faker.Address.secondary_address(),
+      city: Faker.Address.city(),
+      state: Faker.Address.state(),
+      country: order.payment_country,
+      postal_code: Faker.Address.postcode(),
+      phone_number: Faker.Phone.EnUs.phone()
+    }
+  end
+
+  def shipping_address_factory do
+    order = order_factory()
+
+    %ShippingAddress{
+      first_line: Faker.Address.street_address(),
+      second_line: Faker.Address.secondary_address(),
+      city: Faker.Address.city(),
+      state: Faker.Address.state(),
+      country: order.payment_country,
+      postal_code: Faker.Address.postcode(),
+      phone_number: Faker.Phone.EnUs.phone()
+    }
+  end
+
+  def order_factory do
+    order_price = Enum.random(20_000..1_000_000)
+    tax_price = (order_price * 0.19) |> floor
+    payment_method = Enum.random(["EFECTY", "PSE", "BANK_REFERENCED"])
+    country_code = Faker.Address.country_code()
+
+    %Order{
+      total_transaction_price: order_price + tax_price,
+      tax_price: tax_price,
+      order_price: order_price,
+      currency: Enum.random(["COP", "ARS", "BRL", "CLP", "MXN", "PEN", "USD"]),
+      payment_installments: Enum.random(1..8),
+      payment_method: payment_method,
+      payment_country: country_code,
+      skus: %{
+        Faker.Address.country_code() => to_string(Enum.random(1..5))
+      },
+      status: Enum.random(Order.order_statuses())
+    }
+  end
+
+  def static_value_order_factory do
+    order_price = Enum.random(20_000..1_000_000)
+    tax_price = (order_price * 0.19) |> floor
+    country_code = Faker.Address.country_code()
+
+    %Order{
+      total_transaction_price: order_price + tax_price,
+      tax_price: tax_price,
+      order_price: order_price,
+      currency: "GBP",
+      payment_installments: 10,
+      payment_method: "Credit Card",
+      payment_country: country_code,
+      skus: %{
+        Faker.Address.country_code() => to_string(Enum.random(1..5))
+      },
+      status: "preparing_for_shipping"
+    }
+  end
+
+  def order_from_last_month_factory do
+    order_price = Enum.random(20_000..1_000_000)
+    tax_price = (order_price * 0.19) |> floor
+    payment_method = Enum.random(["EFECTY", "PSE", "BANK_REFERENCED"])
+    country_code = Faker.Address.country_code()
+
+    %Order{
+      total_transaction_price: order_price + tax_price,
+      tax_price: tax_price,
+      order_price: order_price,
+      currency: Enum.random(["COP", "ARS", "BRL", "CLP", "MXN", "PEN", "USD"]),
+      payment_installments: Enum.random(1..8),
+      payment_method: payment_method,
+      payment_country: country_code,
+      skus: %{
+        Faker.Address.country_code() => to_string(Enum.random(1..5))
+      },
+      status: Enum.random(Order.order_statuses()),
+      inserted_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000]),
+      updated_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000])
+    }
+  end
+
+  def order_with_cart_id_factory do
+    order_price = Enum.random(20_000..1_000_000)
+    tax_price = (order_price * 0.19) |> floor
+    payment_method = Enum.random(["EFECTY", "PSE", "BANK_REFERENCED"])
+    country_code = Faker.Address.country_code()
+    cart = insert(:cart)
+
+    %Order{
+      total_transaction_price: order_price + tax_price,
+      tax_price: tax_price,
+      order_price: order_price,
+      currency: Enum.random(["COP", "ARS", "BRL", "CLP", "MXN", "PEN", "USD"]),
+      payment_installments: Enum.random(1..8),
+      payment_method: payment_method,
+      payment_country: country_code,
+      skus: %{
+        Faker.Address.country_code() => to_string(Enum.random(1..5))
+      },
+      status: Enum.random(Order.order_statuses()),
+      cart_id: cart.id
+    }
+  end
+
+  def payment_method_factory do
+    order = order_factory()
+
+    %PaymentMethod{
+      name: order.payment_method,
+      cc_token_id: Faker.UUID.v4()
+    }
+  end
+
+  def user_factory do
+    %User{
+      name: Faker.Person.PtBr.name(),
+      inserted_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000]),
+      updated_at: NaiveDateTime.new!(~D[2022-10-01], ~T[04:00:00.000])
+    }
+  end
+end
